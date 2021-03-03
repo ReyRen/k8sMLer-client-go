8c46a67fbc0d37c2d179612e5cafc31382a4a658
ReyRen <reyren179@gmail.com>
2021-03-03 19:11:56 +0800

    part 5 for hot upgrade node label/status to .update map

--- common.go
+++ common.go
@@ -541,19 +541,22 @@ func (c *Client) recordToUpdate(statusCode int) {
 	}
 
 	//handle map {"4-129":["zz8k5nsfzv0jljl","2","node3-1,node2-1,node2-1","gpu","8","0"]}
-	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], c.hub.clients[*c.userIds].Head.rm.RandomName)
-	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], strconv.Itoa(c.hub.clients[*c.userIds].Head.rm.Type))
-	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], c.hub.clients[*c.userIds].Head.rm.Content.ResourceType)
+	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], c.hub.clients[*c.userIds].Head.rm.RandomName)                             //0
+	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], strconv.Itoa(c.hub.clients[*c.userIds].Head.rm.Type))                     //1
+	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], c.hub.clients[*c.userIds].Head.rm.Content.ResourceType)                   //2
+	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], c.hub.clients[*c.userIds].Head.sm.Content.ResourceInfo.NodesListerName)   //3
+	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], c.hub.clients[*c.userIds].Head.sm.Content.ResourceInfo.NodesListerLabel)  //4
+	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], c.hub.clients[*c.userIds].Head.sm.Content.ResourceInfo.NodesListerStatus) //5
 
 	var selectedNodes []string
 	for _, v := range *(c.hub.clients[*c.userIds].Head.rm.Content.SelectedNodes) {
 		selectedNodes = append(selectedNodes, v.NodeNames+"-"+strconv.Itoa(v.GPUNum))
 	}
-	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], strings.Join(selectedNodes, ","))
+	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], strings.Join(selectedNodes, ",")) //6
 
-	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], strconv.Itoa(statusCode)) // socket statusId
+	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], strconv.Itoa(statusCode)) // socket statusId //7
 
-	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], "") // updated:"", not updated:"1"
+	UPDATEMAP[mapKey] = append(UPDATEMAP[mapKey], "") // updated:"", not updated:"1" //8
 
 	//dataReady, err := json.MarshalIndent(UPDATEMAP, "", " ")
 	dataReady, err := json.Marshal(UPDATEMAP)
@@ -604,7 +607,7 @@ func (c *Client) reloadUpdateInfo(mod string) {
 	if _, ok := UPDATEMAP[mapKey]; !ok {
 		//new
 		Trace.Printf("[%d. %d] is a new connection, exit [%s] mode validation program", c.userIds.Uid, c.userIds.Tid, mod)
-	} else if UPDATEMAP[mapKey][5] == "" { // for index out of range UPDATEMAP[mapKey][5] error
+	} else if UPDATEMAP[mapKey][8] == "" { // for index out of range UPDATEMAP[mapKey][5] error
 		//new
 		Trace.Printf("[%d. %d] is a new connection, exit [%s] mode validation program", c.userIds.Uid, c.userIds.Tid, mod)
 	} else {
@@ -613,17 +616,20 @@ func (c *Client) reloadUpdateInfo(mod string) {
 		c.hub.clients[*c.userIds].Head.rm.RandomName = UPDATEMAP[mapKey][0]
 		c.hub.clients[*c.userIds].Head.rm.Type, _ = strconv.Atoi(UPDATEMAP[mapKey][1])
 		c.hub.clients[*c.userIds].Head.rm.Content.ResourceType = UPDATEMAP[mapKey][2]
+		c.hub.clients[*c.userIds].Head.sm.Content.ResourceInfo.NodesListerName = UPDATEMAP[mapKey][3]
+		c.hub.clients[*c.userIds].Head.sm.Content.ResourceInfo.NodesListerLabel = UPDATEMAP[mapKey][4]
+		c.hub.clients[*c.userIds].Head.sm.Content.ResourceInfo.NodesListerStatus = UPDATEMAP[mapKey][5]
 		//handle selectednodes
 		var i int
 		i = 0
-		for _, v := range strings.Split(UPDATEMAP[mapKey][3], ",") {
+		for _, v := range strings.Split(UPDATEMAP[mapKey][6], ",") {
 			(*(c.hub.clients[*c.userIds].Head.rm.Content.SelectedNodes))[i].NodeNames = strings.Split(v, "-")[0]
 			(*(c.hub.clients[*c.userIds].Head.rm.Content.SelectedNodes))[i].GPUNum, _ = strconv.Atoi(strings.Split(v, "-")[1])
 			i++
 		}
-		statusCode, _ := strconv.Atoi(UPDATEMAP[mapKey][4])
+		statusCode, _ := strconv.Atoi(UPDATEMAP[mapKey][7])
 
-		UPDATEMAP[mapKey][5] = "" // updated, reset to null(need to manually set "" to "1" in .update file)
+		UPDATEMAP[mapKey][8] = "" // updated, reset to null(need to manually set "" to "1" in .update file)
 
 		// active logs
 		if statusCode >= RESOURCECOMPLETE {
